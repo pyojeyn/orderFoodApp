7fd75a81b1cac7b1cf7d8d25b69aa804c00acad4
pyojeyn <jeynpyo@gmail.com>
2023-06-15 21:25:44 +0900

    장바구니 수량 +,- 작업

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,15 +9,60 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if(action.type === 'ADD'){
-        const updatedItems = state.items.concat(action.item);
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id); // 인덱스
+        const existingCartItem = state.items[existingCartItemIndex] // 인덱스로 해당 요소 가져오기.
+        
+        let updatedItems;
+
+        if(existingCartItem) {
+           const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem; // 새로 덮어씌어주기. 
+        }else {
+            // 새로운 항목 
+           updatedItems = state.items.concat(action.item);
+        }
+
+        
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         };
-    }  
+    };  
 
-    return 
+    if(action.type === 'REMOVE') {
+    
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.id
+        ); // 인덱스
+
+        console.log(existingCartItemIndex)
+
+        const existingItem = state.items[existingCartItemIndex] // 인덱스로 해당 요소 가져오기.
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if(existingItem.amount === 1){
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        }else{
+            const updatedItem = { ...existingItem, amount:existingItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
+
+    return defaultCartState;
 };
 
 
